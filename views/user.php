0602221860eda936e145baceeb63b4b319659a65
nuragnia <0110124146@student.nurulfikri.ac.id>
2025-06-12 13:48:38 +0700

    praktiku06

---
+++ views/user.php
@@ -0,0 +1,21 @@
+<section class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Telpon</th>
+                                <th>Email</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
